f1645ce8ac51aedf4eab2a6994ef131528658fe9
jcislemadev-coder <jcislemadev@gmail.com>
2026-04-17 04:10:06 -0500

    mover a la izquierda y mover derecha

--- limones.js
+++ limones.js
@@ -3,6 +3,8 @@ let ctx=canvas.getContext("2d");
 const ALTURA_SUELO = 20;
 const ALTURA_PERSONAJE =120; 
 const ANCHO_PERSONAJE = ALTURA_PERSONAJE/2;
+let personajeX=canvas.width/2;
+let personajeY=canvas.height;
 
 function dibujarSuelo (){
     ctx.fillStyle = "#ff0000";
@@ -11,7 +13,7 @@ function dibujarSuelo (){
 
 function dibujarPersonaje(){
     ctx.fillStyle = "#5f9b26";
-    ctx.fillRect(canvas.width-320,canvas.height-(ALTURA_SUELO+ALTURA_PERSONAJE),ANCHO_PERSONAJE,ALTURA_PERSONAJE);
+    ctx.fillRect(personajeX,personajeY-(ALTURA_SUELO+ALTURA_PERSONAJE),ANCHO_PERSONAJE,ALTURA_PERSONAJE);
 }
 
 function iniciar(){
@@ -19,3 +21,22 @@ function iniciar(){
     dibujarPersonaje();
 }
 
+function moverIzquierda(){
+    actualizarPantalla();
+    personajeX  = personajeX -10;
+}
+
+function moverDerecha() {
+    actualizarPantalla();
+    personajeX = personajeX +10;
+}
+
+function actualizarPantalla(){
+    limpiarCanva();
+    dibujarSuelo();
+    dibujarPersonaje();
+}
+
+function limpiarCanva(){
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+}
